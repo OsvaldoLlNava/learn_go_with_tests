4ee5d458b7c33a915c04ce3e68ab9051a4c6d51b
Osvaldo Nava <lllanes@tredicom.com>
2021-07-07 10:30:45 -0500

    se realizo refactor y se agrego un seungo test para hello

--- 01_hello_world/hello_test.go
+++ 01_hello_world/hello_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Osvaldo")
-	want := "Hello, Osvaldo"
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("Saying Hello to people", func(t *testing.T) {
+		got := Hello("Osvaldo")
+		want := "Hello, Osvaldo"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, Queso"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
