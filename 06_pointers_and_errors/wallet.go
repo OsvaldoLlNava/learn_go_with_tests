baaa8f482d4e6bb859b8a66babd1f4482bf78a4a
OsvaldoLlNava <luis_llanesn@hotmail.com>
2021-07-08 17:05:27 -0500

    add methods to wallet struct and new test without refactor

--- 06_pointers_and_errors/wallet.go
+++ 06_pointers_and_errors/wallet.go
@@ -1,13 +1,25 @@
 package wallet
 
+import "fmt"
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 type Wallet struct {
-	Money int
+	balance Bitcoin
 }
 
-func (w Wallet) Deposit(cantidad int) {
-
+func (w *Wallet) Deposit(cantidad Bitcoin) {
+	w.balance += cantidad
 }
 
-func (w Wallet) Balance() int {
-	return 0
+func (w *Wallet) Withdraw(cantidad Bitcoin) {
+	w.balance -= cantidad
+}
+
+func (w Wallet) Balance() Bitcoin {
+	return w.balance
 }
