495dc843d4d1e64a5f587839241eb76b632aaf2e
OsvaldoLlNava <luis_llanesn@hotmail.com>
2021-07-09 12:05:44 -0500

    add an check erro in a first withdraw test

--- 06_pointers_and_errors/wallet_test.go
+++ 06_pointers_and_errors/wallet_test.go
@@ -15,8 +15,9 @@ func TestWallet(t *testing.T) {
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
 
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
@@ -47,5 +48,12 @@ func assertError(t testing.TB, got error, want error) {
 
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
 	}
 }
